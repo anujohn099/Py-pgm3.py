4c5d86596ee97d4d4624f846f352333a65d0640c
anujohn099 <anu252135@gmail.com>
2024-11-25 18:16:33 +0530

    Create If- elif- else .py

---
+++ If- elif- else .py
@@ -0,0 +1,8 @@
+#write a python program to accept salary and cakculate tax amount 
+#condition 1: if the salary is less than or equal to 50000 tax=0.05*salary
+
+#condition 2: if the salary is less than or equal to 60000 tax=0.07*salary
+
+#condition 3: if the salary is less than or equal to 70000 tax=0.08*salary
+
+#condition 4: or tax=0.10*salary
